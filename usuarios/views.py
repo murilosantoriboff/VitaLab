c626f6f7b39116a7e180b954d3aa5ae766020388
Murilo Santori Boff <murilosantori45@gmail.com>
2023-10-04 20:29:26 -0300

    Finalizando Login e Register starting exame

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib.auth import authenticate,login
 
 def cadastro(request):
     if request.method == "GET":
@@ -38,3 +39,20 @@ def cadastro(request):
 
         messages.add_message(request, constants.SUCCESS, 'Usuario criado com sucesso')
         return redirect('/usuarios/cadastro')
+    
+def logar(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = authenticate(username=username, password=senha)
+
+        if user:
+            login(request, user)
+		    # Acontecerá um erro ao redirecionar por enquanto, resolveremos nos próximos passos
+            return redirect('/')
+        else:
+            messages.add_message(request, constants.ERROR, 'Usuario ou senha inválidos')
+            return redirect('/usuarios/login')
