931ecaa1d9501aacff669503b546a55ddbceb938
Murilo Santori Boff <murilosantori45@gmail.com>
2023-10-07 07:43:43 -0300

    exames finishing

--- exames/urls.py
+++ exames/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('solicitar_exames/', views.solicitar_exames, name="solicitar_exames"),
+    path('fechar_pedido/', views.fechar_pedido, name="fechar_pedido"),
 ]
--- exames/views.py
+++ exames/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import TiposExames
+from .models import TiposExames, PedidosExames, SolicitacaoExame
+from datetime import datetime
+from django.contrib.messages import constants
+from django.contrib import messages
 
 @login_required
 def solicitar_exames(request):
@@ -14,3 +17,29 @@ def solicitar_exames(request):
         for i in solicitacao_exames:
             preco_total += i.preco
         return render(request, 'solicitar_exames.html', {'solicitacao_exames': solicitacao_exames, 'preco_total': preco_total, 'tipos_exames': tipos_exames})
+
+@login_required
+def fechar_pedido(request):
+    exames_id = request.POST.getlist('exames')
+    solicitacao_exames = TiposExames.objects.filter(id__in=exames_id)
+
+    pedido_exame = PedidosExames(
+        usuario = request.user,
+        data = datetime.now()
+    )
+
+    pedido_exame.save()
+
+    for exame in solicitacao_exames:
+        solicitacao_exames_temp = SolicitacaoExame(
+            usuario=request.user,
+            exame=exame,
+            status="E"
+        )
+        solicitacao_exames_temp.save()
+        pedido_exame.exames.add(solicitacao_exames_temp)
+        
+    pedido_exame.save()
+
+    messages.add_message(request, constants.SUCCESS, 'Pedido de exame concluído com sucesso')
+    return redirect('/exames/ver_pedidos/')
