997af7690ca772d475c187c6d7186ca63177cc42
Murilo Santori Boff <murilosantori45@gmail.com>
2023-10-09 20:36:33 -0300

    Cancelar Pedido

--- exames/views.py
+++ exames/views.py
@@ -42,4 +42,21 @@ def fechar_pedido(request):
     pedido_exame.save()
 
     messages.add_message(request, constants.SUCCESS, 'Pedido de exame concluído com sucesso')
-    return redirect('/exames/ver_pedidos/')
+    return redirect('/exames/gerenciar_pedidos/')
+
+@login_required
+def gerenciar_pedidos(request):
+    pedidos_exames = PedidosExames.objects.filter(usuario=request.user)
+    return render(request, 'gerenciar_pedidos.html', {'pedidos_exames': pedidos_exames})
+
+def cancelar_pedido(request, pedido_id):
+    pedido = PedidosExames.objects.get(id=pedido_id)
+
+    if not pedido.usuario == request.user:
+        messages.add_message(request, constants.ERROR, 'Esse pedido não é seu')
+        return redirect('/exames/gerenciar_pedidos/')
+
+    pedido.agendado = False
+    pedido.save()
+    messages.add_message(request, constants.SUCCESS, 'Pedido excluido com sucesso')
+    return redirect('/exames/gerenciar_pedidos/')
